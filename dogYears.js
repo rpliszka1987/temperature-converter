a39df18eede91c57b5b7782b258ba9ec5deb6ca6
Robert Pliszka <robplisz@gmail.com>
2022-08-26 00:00:27 -0400

    Added a JS project which calculates your age in dog years

---
+++ dogYears.js
@@ -0,0 +1,16 @@
+// Assigining my age to the variable
+let myAge = 36;
+// Assigining earlyYears variable to 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Subtracting earlyYears from myAge variable
+let laterYears = myAge - 2;
+// Multiply variable laterYears by 4 to get dog age
+laterYears *= 4;
+// Calculatiung the age in dog years.
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Convert name to lowecase letters
+let myName = "Robert".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
